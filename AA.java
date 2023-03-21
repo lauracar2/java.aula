5b08ad81d644f33df283abf7d467b5935585750d
Laura Carvalho <127751470+lauracar2@users.noreply.github.com>
2023-03-21 10:26:18 -0300

    Add files via upload

---
+++ AA.java
@@ -0,0 +1,44 @@
+package jv;
+
+import java.util.Scanner;
+
+public class AA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		double alcool = 2.90;
+		double gasolina = 3.79;
+
+		System.out.println("A - gaslina");
+		System.out.println("B - alcool");
+		System.out.println("Qual o tipo de combustivel? ");
+		String tipo = sc.nextLine();
+		System.out.println("Informe a capacidade do tanque: ");
+		double capacidade = sc.nextDouble();
+
+		if (tipo.equals("g") || tipo.equals("G")) {
+
+			double tanque = gasolina * capacidade;
+			System.out.println("O valor total para encher o tanque é de " + tanque);
+
+		}
+
+		if (tipo.equals("a") || tipo.equals("A")) {
+
+			double tanque = alcool * capacidade;
+			System.out.println("O valor total para encher o tanque é de " + tanque);
+
+		} 
+		
+		else {
+			System.out.println(" opção inválida!!");
+
+		}
+		
+		sc.close();
+	}
+
+}
