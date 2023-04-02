3542d2f8ea22d8207dcc45419d61468fc564cf34
Laura Carvalho <127751470+lauracar2@users.noreply.github.com>
2023-04-02 13:59:22 -0300

    Add files via upload

---
+++ LA.java
@@ -0,0 +1,17 @@
+
+public class LA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		double v, a, r;
+		
+		r = 3.5;
+		a = 4.3;
+		
+		v = 3.14159 * r * r * a;
+		
+		System.out.println(" O valor da lata de óleo é de " + v + "m3" );
+	}
+
+}
