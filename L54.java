bfd4a9263812ef6e3e03a0ffdca3e63bc39c9c35
Laura Carvalho <127751470+lauracar2@users.noreply.github.com>
2023-04-02 13:05:10 -0300

    Add files via upload

---
+++ L54.java
@@ -0,0 +1,22 @@
+
+public class L54 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// calcule o salário de 4 funcionários e dê a média //
+		double  n1, n2, n3, n4, media;
+		
+		n1 = 1880.00;
+		n2 = 14300.45;
+		n3 = 1350.90;
+		n4 = 13423.00;
+		
+	    media = (n1 + n2 + n3 + n4)/2 ;
+	     
+	    System.out.println(" A média salárial da eempresaa é de: " + media);
+	    
+	    
+	}
+
+}
