48a7ee8fdcec6ddb9d0416563022b0157fdeb577
Laura Carvalho <127751470+lauracar2@users.noreply.github.com>
2023-04-05 20:33:07 -0300

    Calculo de quantidade de cavalos

---
+++ LAU.java
@@ -0,0 +1,21 @@
+
+public class LAU {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Calculo de quantidade de cavalos //
+		
+		double m, h, t, cavalos;
+		
+		m = 23;
+		h = 1.4;
+		t = 2.3;
+		
+		cavalos = (m * h * t)/ 745.699;
+		System.out.println("A quantidade de cavalos é de " + cavalos);
+		
+		
+	}
+
+}
